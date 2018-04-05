b1ac639a156df5f3429d164c58640d83ed3906c9
Marian Serna <marianhalliday@gmail.com>
2018-04-05 17:54:31 -0400

    Creating form + Setting up firebase

--- src/App.js
+++ src/App.js
@@ -1,67 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends React.Component {
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAixAhfej2WHna1z65Q5fTc2b9rPABdgi4',
+      authDomain: 'reactnativeauth-22aba.firebaseapp.com',
+      databaseURL: 'https://reactnativeauth-22aba.firebaseio.com',
+      projectId: 'reactnativeauth-22aba',
+      storageBucket: 'reactnativeauth-22aba.appspot.com',
+      messagingSenderId: '727195793728'
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>Thing of beauty</Text>
+        <Header headerText="Thing of Beauty 🐸" />
+        <LoginForm />
       </View>
     );
   }
 }
 
 export default App;
-
-// import React, { Component } from 'react';
-// import {
-//   Platform,
-//   StyleSheet,
-//   Text,
-//   View
-// } from 'react-native';
-
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' +
-//     'Cmd+D or shake for dev menu',
-//   android: 'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu',
-// });
-
-// type Props = {};
-// export default class App extends Component<Props> {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>
-//           Welcome to React Native!
-//         </Text>
-//         <Text style={styles.instructions}>
-//           To get started, edit App.js
-//         </Text>
-//         <Text style={styles.instructions}>
-//           {instructions}
-//         </Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
---
+++ src/components/LoginForm.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { Button, Card, CardSection, Input } from './common';
+
+class LoginForm extends React.Component {
+  state = {
+    email: '',
+    password: ''
+  };
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+            label="Email"
+            placeholder="user@gmail.com"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+            label="Password"
+            placeholder="password"
+            secureTextEntry
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>Log in</Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+export default LoginForm;
